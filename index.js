661bc15c5bdb422c7be2bc514f5e50c4a39c4788
doniozan <32379314+doniozan@users.noreply.github.com>
2017-10-22 18:52:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.use('/', function ( req, res, next) {
 });
 let loginRoute = require('./login/loginRoute');
 app.use('/api',loginRoute);
+let pesertaRoute = require('./peserta/pesertaRoute');
+app.use('/api',pesertaRoute);
+let angkatanAktualRoute = require('./angkatanAktual/angkatanAktualRoute');
+app.use('/api',angkatanAktualRoute);
+let angkatanRencanaRoute = require('./angkatanRencana/angkatanRencanaRoute');
+app.use('/api',angkatanRencanaRoute);
 mong.connect('mongodb://doni:h4g4t4doniozan@ds113835.mlab.com:13835/dbpelatihan');
 app.listen(process.env.PORT || 8889, function() {
   console.log('Node app is running on port', app.get('port'));
